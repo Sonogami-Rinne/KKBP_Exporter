5b7856d442ff769a14e91984e373412d1cf8ce53
FlailingFog <65811931+FlailingFog@users.noreply.github.com>
2025-01-04 11:39:53 -0500

    Add bool for hair shaders in MatDataComplete.json

--- KKBP_Exporter/MaterialInfo.cs
+++ KKBP_Exporter/MaterialInfo.cs
@@ -12,8 +12,9 @@ internal class MaterialInfo
 
 	public List<string> ShaderPropNames = new List<string>();
 	public List<Color> ShaderPropColorValues = new List<Color>();
+    public bool isHair = false;
 
-	public MaterialInfo(Material material, string _materialName)
+    public MaterialInfo(Material material, string _materialName)
 	{
 		MaterialName = _materialName;
 		ShaderName = material.shader.name;
@@ -24,12 +25,27 @@ internal class MaterialInfo
 		}
 		foreach (MaterialShaderItem property in materialShader2.properties)
 		{
-			string text = "_" + property.name;
+			// Add the color data
+            string text = "_" + property.name;
 			if (property.type == "Color")
 			{
 				ShaderPropNames.Add(text + " " + property.type + " " + ShaderPropColorValues.Count());
 				ShaderPropColorValues.Add(material.GetColor(text));
-			}
-		}
-	}
+            }
+
+			// check the names of each texture. if there is a texture with "_HGLS" in the name, mark it as a hair shader.
+            if (property.type == "Texture")
+            {
+                Dictionary<string, string> dictionary = PmxBuilder.typeMap.ToDictionary((KeyValuePair<string, string> x) => x.Value, (KeyValuePair<string, string> x) => x.Key);
+                if (dictionary.ContainsKey(text))
+                {
+                    string text2 = dictionary[text];
+					if (text2 == "_HGLS") {
+						isHair = true;
+                    }
+                }
+            }
+
+        }
+    }
 }
